1ae3b7fbb695cba2174a21c001ed7401e2bea20b
parakhchoudhary1208 <0808cs181119.ies@ipsacademy.org>
2025-10-29 15:42:18 +0530

    hygined page tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,20 @@
+import React from 'react'
 import AboutSection from '@/components/about-section'
 import ContactSection from '@/components/contact-section'
 import Footer from '@/components/footer'
-import Header from '@/components/header'
 import HeroSection from '@/components/hero-section'
 import ProjectsSection from '@/components/projects-section'
-import SectionDivider from '@/components/section-divider'
 import { ThemeToggle } from '@/components/theme-toggle'
 
 export default function Home() {
   return (
     <>
       <div className="max-w-[1440px] mx-auto flex flex-col items-center px-5 md:px-10">
-        <HeroSection />
-        <AboutSection />
-        {/* <SectionDivider />
-        <SkillsSection/> */}
-        {/* <SectionDivider />
-        <ExperienceSection /> */}
-        <SectionDivider />
-        <ProjectsSection />
-        <ContactSection />
-        <Footer />
+        <HeroSection/>
+        <AboutSection/>
+        <ProjectsSection/>
+        <ContactSection/>
+        <Footer/>
       </div>
       {/* Desktop: Theme Toggle */}
       <ThemeToggle className="bg-background hidden sm:fixed sm:bottom-8 sm:right-8 sm:flex" />
