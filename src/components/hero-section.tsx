f49d9722b58f9276d73f4d15d9172b528b4d474d
parakhchoudhary1208 <0808cs181119.ies@ipsacademy.org>
2025-10-29 15:19:18 +0530

    removed line

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -15,7 +15,7 @@ export default function HeroSection() {
     <section
       ref={ref}
       id="home"
-      className="relative flex flex-col items-center justify-center gap-5 text-center h-dvh"
+      className="relative flex flex-col items-center justify-center gap-5 text-center h-[80vh]"
     >
       <GridPattern
         width={80}
--- src/components/section-divider.tsx
+++ src/components/section-divider.tsx
@@ -8,7 +8,7 @@ export default function SectionDivider() {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.125 }}
-      className="bg-muted my-6 h-8 w-1 rounded-full sm:my-12"
+      className="bg-transparent my-6 h-8 w-1 rounded-full sm:my-12"
     ></motion.div>
   )
 }
